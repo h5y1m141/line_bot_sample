a8f07153ca1ed402f0f6fe092dd32fcf6385a64f
HiroshiOyamada <h5y1m141@gmail.com>
2019-12-04 09:11:07 +0900

    LINE Botとのやりとり時に必要なデーターを保存する処理を追加

--- app/controllers/line/messages_controller.rb
+++ app/controllers/line/messages_controller.rb
@@ -13,7 +13,7 @@ module Line
       profile = LineProfileRepository.find_by(line_user_id: line_user_id)
       line_display_name = profile['displayName']
 
-      LineProfileRepository.create!(
+      line_user = LineProfileRepository.create(
         line_user_id: line_user_id,
         line_display_name: line_display_name
       )
@@ -21,11 +21,43 @@ module Line
       events.each do |event|
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: "#{line_display_name}さん、こんにちは!!id: #{line_user_id}"
-          }
-          @client.reply_message(event['replyToken'], message)
+          reply_token = event['replyToken']
+          message_sequence_id ||= SecureRandom.hex(8)
+          if event.message['text'].match(/.*予約/).present?
+            line_message = LineUserMessage.create!(
+              message_sequence_id: message_sequence_id,
+              line_user_id: line_user.id,
+              reply_token: reply_token,
+              message: event.message['text'],
+              status: LineUserMessage::ACCEPT_REQUEST
+            )
+            message = {
+              type: 'text',
+              text: 'ランチかディナーどちらにしますか？'
+            }
+          elsif event.message['text'].match(/.*ランチ|ディナー/).present?
+            line_message = LineUserMessage.where(
+              line_user_id: line_user.id,
+              status: LineUserMessage::ACCEPT_REQUEST
+            ).last
+            LineUserMessage.create!(
+              message_sequence_id: line_message.message_sequence_id,
+              line_user_id: line_user.id,
+              reply_token: reply_token,
+              message: event.message['text'],
+              status: LineUserMessage::ASK_VISIT_DATETIME
+            )
+            message = {
+              type: 'text',
+              text: "それでは次に人数を教えて下さい"
+            }
+          else
+            message = {
+              type: 'text',
+              text: "#{line_display_name}さん、申し訳ありませんがそのような内容は受付られません。"
+            }
+          end
+          @client.reply_message(reply_token, message)
         end
       end
 
--- app/repositories/line_profile_repository.rb
+++ app/repositories/line_profile_repository.rb
@@ -4,16 +4,17 @@ class LineProfileRepository
       LineBotApi::GetProfile.new.execute(line_user_id: line_user_id)
     end
 
-    def create!(line_user_id:, line_display_name:)
+    def create(line_user_id:, line_display_name:)
       line_user = LineUser.find_by(line_user_id: line_user_id)
       if line_user.present?
-        line_user.update!(line_display_name: line_display_name)
+        line_user.update(line_display_name: line_display_name)
       else
-        LineUser.create!(
+        line_user = LineUser.create(
           line_user_id: line_user_id,
           line_display_name: line_display_name
         )
       end
+      line_user
     end
   end
 end
